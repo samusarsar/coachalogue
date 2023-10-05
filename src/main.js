847f2a285af3285bba89188f723512aa4d586515
ssarandev <samuil.sarandev@dxc.com>
2023-10-05 17:12:03 +0300

    Fix initial project setup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-import './assets/main.css'
+import { createApp } from 'vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp({}).mount('#app');
