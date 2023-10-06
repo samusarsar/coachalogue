f4532152bdb967dd45a3816da1d1cb8f0f07c5ec
ssarandev <samuil.sarandev@dxc.com>
2023-10-06 15:30:10 +0300

    Add all base page routes and nested links

--- src/pages/coaches/CoachesDetails.vue
+++ src/pages/coaches/CoachesDetails.vue
@@ -1,5 +1,7 @@
 <template>
   <div>Coaches Details</div>
+  <router-view></router-view>
+  <router-link to="/coaches/:id/contact">Contact</router-link>
 </template>
 
 <script>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Coaches List</div>
+  <section>
+    <div>FILTER</div>
+  </section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register</router-link>
+    </div>
+    <ul>
+      Coaches List
+    </ul>
+  </section>
 </template>
 
 <script>
