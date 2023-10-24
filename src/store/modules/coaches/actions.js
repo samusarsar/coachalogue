b4fa9fd7e40e6a33605d2adc374019d121c221f6
ssarandev <samuil.sarandev@dxc.com>
2023-10-24 18:17:46 +0300

    Add coach list fetching from database

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -28,4 +28,32 @@ export default {
       id: userId,
     });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coachalogue-6a3db-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log('hit');
+    }
+    console.log(responseData);
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].rate,
+        areas: responseData[key].areas,
+      };
+
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
+  },
 };
