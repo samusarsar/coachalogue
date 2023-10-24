efce72003842aac45ec39fbaec8e4eefdebfa8c2
ssarandev <samuil.sarandev@dxc.com>
2023-10-24 18:52:31 +0300

    Add fetching, loaders, and error handling for requests received

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,12 +1,55 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
 
+    const response = await fetch(
+      `https://coachalogue-6a3db-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      throw new Error(responseData.message || 'Failed to send request!');
+    }
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+
+    const response = await fetch(
+      `https://coachalogue-6a3db-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      throw new Error(responseData.message || 'Failed to fetch requests!');
+    }
+
+    const requests = [];
+
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: responseData[key].userEmail,
+        message: responseData[key].message,
+      };
+
+      requests.push(request);
+    }
+
+    context.commit('setRequests', requests);
   },
 };
