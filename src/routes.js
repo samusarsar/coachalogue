182160f51c0d7939e68ba195e4b74fce9770e29f
ssarandev <samuil.sarandev@dxc.com>
2023-10-25 16:11:45 +0300

    Add base UserAuth component

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import CoachesDetails from './pages/coaches/CoachesDetails.vue';
 import CoachRegister from './pages/coaches/CoachRegister.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 import NotFound from './pages/NotFound.vue';
 
 export const router = createRouter({
@@ -25,6 +26,7 @@ export const router = createRouter({
     },
     { path: '/register', component: CoachRegister },
     { path: '/requests', component: RequestsReceived },
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
